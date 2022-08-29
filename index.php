cb27046eccb18ed9b19dc544527691b602182dc0
Bijay Thakur <bijaythakur330@gmail.com>
2022-08-29 15:34:06 +0545

    initial commits 121 - Creating a Plugin

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name:       Udemy Plus
+ * Plugin URI:        https://udemy.com
+ * Description:       A plugin for adding blocks to a theme.
+ * Version:           1.0.0
+ * Requires at least: 5.9
+ * Requires PHP:      7.2
+ * Author:            Bijay Thakur
+ * Author URI:        https:bijaythakur.com/
+ * Text Domain:       udemy-plus
+ * Domain Path:       /languages
+ */
