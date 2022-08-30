3c74cf3d827ddeb6ad1ce834a9fae80ffb9ffc71
Bijay Thakur <bijaythakur330@gmail.com>
2022-08-30 11:13:39 +0545

    141 - Attribute Sources

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b9c6a7041087f5c9a46e');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '848989712f6b1f5e44c3');
