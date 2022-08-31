398c963347d618e6248965bb5e942f45b9d44547
Bijay Thakur <bijaythakur330@gmail.com>
2022-08-31 09:33:25 +0545

    147 - The PanelColorSettings Component

--- build/blocks/search-form/index.asset.php
+++ build/blocks/search-form/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => 'a8acd8aa7eab1c1f8af3');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '7302ef1f9e7c68420545');
--- src/blocks/search-form/index.js
+++ src/blocks/search-form/index.js
@@ -1,23 +1,48 @@
 import { registerBlockType } from '@wordpress/blocks'
-import { useBlockProps } from '@wordpress/block-editor'
+import {
+  useBlockProps,
+  PanelColorSettings,
+  InspectorControls,
+} from '@wordpress/block-editor'
+import { __ } from '@wordpress/i18n'
 import block from './block.json'
 import icons from '../../icons'
 import './main.css'
 
 registerBlockType(block.name, {
   icon: icons.primary,
-  edit() {
+  edit({ attributes, setAttributes }) {
+    const { bgColor, textColor } = attributes
     const blockProps = useBlockProps()
     return (
-      <div {...blockProps}>
-        <h1>Search: Your search term here</h1>
-        <form>
-          <input type='text' placeholder='Search' />
-          <div className='btn-wrapper'>
-            <button type='submit'>Search</button>
-          </div>
-        </form>
-      </div>
+      <>
+        <InspectorControls>
+          <PanelColorSettings
+            title={__('Colors', 'udemy-plus')}
+            colorSettings={[
+              {
+                label: __('Background Color', 'udemy-plus'),
+                value: bgColor,
+                onChange: (newValue) => setAttributes({ bgColor: newValue }),
+              },
+              {
+                label: __('Text Color', 'udemy-plus'),
+                value: textColor,
+                onChange: (newValue) => setAttributes({ textColor: newValue }),
+              },
+            ]}
+          />
+        </InspectorControls>
+        <div {...blockProps}>
+          <h1>Search: Your search term here</h1>
+          <form>
+            <input type='text' placeholder='Search' />
+            <div className='btn-wrapper'>
+              <button type='submit'>Search</button>
+            </div>
+          </form>
+        </div>
+      </>
     )
   },
 })
