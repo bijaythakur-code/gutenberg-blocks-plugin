73a5ae13072b9eb89da39fd0f4c2e95f67a59767
Bijay Thakur <bijaythakur330@gmail.com>
2022-08-31 20:10:52 +0545

    166 - Exercise Toggling the Links

--- build/blocks/header-tools/index.asset.php
+++ build/blocks/header-tools/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e44e7c74b8e4230698ee');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '266986586fbbd9ce9412');
