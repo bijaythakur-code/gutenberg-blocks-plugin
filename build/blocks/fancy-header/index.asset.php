9940e05346fbd05e9beaebaaa35742b15fab5307
Bijay Thakur <bijaythakur330@gmail.com>
2022-08-31 07:53:59 +0545

    145 - Adding a Custom Icon

--- build/blocks/fancy-header/index.asset.php
+++ build/blocks/fancy-header/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5cb9d8edcc83b15825c0');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '56f9d75e352dde6eed49');
--- build/blocks/search-form/index.asset.php
+++ build/blocks/search-form/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element'), 'version' => '895988c412c18becdede');
+<?php return array('dependencies' => array('wp-blocks', 'wp-element'), 'version' => '322c5363f4fd869dd78c');
