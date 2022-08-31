5262df1b9fa82529bd1b0b165f0efa99504821b9
Bijay Thakur <bijaythakur330@gmail.com>
2022-08-31 10:28:41 +0545

    150 - Rendering a Block with Output Buffers

--- includes/blocks/search-form.php
+++ includes/blocks/search-form.php
@@ -2,5 +2,29 @@
 
 function up_search_form_render_cb()
 {
-  return 'Hello!';
+
+  ob_start();
+?>
+
+  <div {...blockProps}>
+    <h1>Search: Your search term here</h1>
+    <form>
+      <input type='text' placeholder='Search' />
+      <div className='btn-wrapper'>
+        <button type='submit' style={{
+                  'background-color': bgColor,
+                  color: textColor,
+                }}>
+          Search
+        </button>
+      </div>
+    </form>
+  </div>
+
+<?php
+
+  $output = ob_get_contents();
+  ob_end_clean();
+
+  return $output;
 }
