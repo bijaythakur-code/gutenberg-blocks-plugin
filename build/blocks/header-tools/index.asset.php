6dd08a76f0b0624164b4c1e4302dacdc180d166e
Bijay Thakur <bijaythakur330@gmail.com>
2022-08-31 19:48:30 +0545

    164 - The SelectControl Component

--- build/blocks/header-tools/index.asset.php
+++ build/blocks/header-tools/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '181274d8719a1864e729');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a08b338663dded2aa5dc');
