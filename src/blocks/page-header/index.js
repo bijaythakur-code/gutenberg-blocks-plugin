d1f6991f63892b0af4d7d756177cf1904e6be8f2
Bijay Thakur <bijaythakur330@gmail.com>
2022-08-31 14:18:52 +0545

    155 - Exercise Adding the RichText Component

--- src/blocks/page-header/index.js
+++ src/blocks/page-header/index.js
@@ -1,16 +1,27 @@
 import { registerBlockType } from '@wordpress/blocks'
-import { useBlockProps } from '@wordpress/block-editor'
+import { useBlockProps, RichText } from '@wordpress/block-editor'
+import { __ } from '@wordpress/i18n'
 import icons from '../../icons.js'
 import './main.css'
 
 registerBlockType('udemy-plus/page-header', {
   icon: icons.primary,
-  edit() {
+  edit({ attributes, setAttributes }) {
+    const { content } = attributes
     const blockProps = useBlockProps()
 
     return (
       <>
-        <div {...blockProps}>Page Header</div>
+        <div {...blockProps}>
+          <div className='inner-page-header'>
+            <RichText
+              tagName='h1'
+              placeholder={__('Heading', 'udemy-plus')}
+              value={content}
+              onChange={(content) => setAttributes({ content })}
+            />
+          </div>
+        </div>
       </>
     )
   },
