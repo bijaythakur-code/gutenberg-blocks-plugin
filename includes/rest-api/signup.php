c5d5f527e24d87f3ecba7c1dbd9e69421e0c54b8
Bijay Thakur <bijaythakur330@gmail.com>
2022-09-01 14:26:50 +0545

    183 - Validating the Request Parameters

--- includes/rest-api/signup.php
+++ includes/rest-api/signup.php
@@ -1,8 +1,18 @@
 <?php
 
-function up_rest_api_signup_handler()
+function up_rest_api_signup_handler($request)
 {
   $response = ['status' => 1];
+  $params = $request->get_json_params();
+
+  if (
+    !isset($params['email'], $params['username'], $params['password']) ||
+    empty($params['email']) ||
+    empty($params['username']) ||
+    empty($params['password'])
+  ) {
+    return $response;
+  }
 
   $response['status'] = 2;
   return $response;
