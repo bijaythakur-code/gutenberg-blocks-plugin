d8df2f4be62ce5f51b10b1a2cc34eaa0b4d7a51c
Bijay Thakur <bijaythakur330@gmail.com>
2022-09-01 11:26:19 +0545

    171 - Loading a View Script

---
+++ build/blocks/auth-model/frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'e181207b39ee4dd49b6b');
---
+++ build/blocks/auth-model/frontend.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*******************************************!*\
+  !*** ./src/blocks/auth-model/frontend.js ***!
+  \*******************************************/
+console.log('Auth Modal Frontend');
+/******/ })()
+;
+//# sourceMappingURL=frontend.js.map
