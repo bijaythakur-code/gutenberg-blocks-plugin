4e9d549fbd343e4f2f9fd6e2fddb49d64191d565
Bijay Thakur <bijaythakur330@gmail.com>
2022-09-02 16:32:57 +0545

    214 - Retrieving the Post ID with Block Context

--- build/blocks/recipe-summary/index.asset.php
+++ build/blocks/recipe-summary/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '2d90fd9f4ffa8914a11f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'ee6edb6cc9d74bf0b69f');
