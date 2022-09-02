38a6dfc61255560a0eaf451a11af2768d762baf0
Bijay Thakur <bijaythakur330@gmail.com>
2022-09-02 14:19:26 +0545

    207 - Flushing Rewrite Rules

--- includes/activate.php
+++ includes/activate.php
@@ -6,4 +6,7 @@ function up_activate_plugin()
   if (version_compare(get_bloginfo('version'), '5.9', '<')) {
     wp_die(__('You must updated WordPress to use this plugin', 'udemy-plus'));
   }
+
+  up_recipe_post_type();
+  flush_rewrite_rules();
 }
