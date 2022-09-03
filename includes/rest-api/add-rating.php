c28c878cda12e31e8444fad0447d3758deb56c11
Bijay Thakur <bijaythakur330@gmail.com>
2022-09-03 14:59:11 +0545

    234 - Inserting the Recipe

--- includes/rest-api/add-rating.php
+++ includes/rest-api/add-rating.php
@@ -28,6 +28,24 @@ function up_rest_api_add_rating_handler($request)
     return $response;
   }
 
+  $wpdb->insert(
+    "{$wpdb->prefix}recipe_ratings",
+    [
+      'post_id' => $postID,
+      'rating'  => $rating,
+      'user_id' => $userID
+    ],
+    ['%d', '%f', '%d']
+  );
+
+  $avgRating = round($wpdb->get_var($wpdb->prepare(
+    "SELECT AVG(`rating`) FROM {$wpdb->prefix}recipe_ratings WHERE post_id=%d",
+    $postID
+  )), 1);
+
+  update_post_meta($postID, 'recipe_rating', $avgRating);
+
   $response['status'] = 2;
+  $response['rating'] = $avgRating;
   return $response;
 }
