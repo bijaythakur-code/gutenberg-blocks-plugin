6b6f8a60fc4d8368c86af7960fe1259f0d33b8cf
Bijay Thakur <bijaythakur330@gmail.com>
2022-09-03 07:58:20 +0545

    222 - Registering Post Metadata

--- includes/recipe-post-type.php
+++ includes/recipe-post-type.php
@@ -41,7 +41,7 @@ function up_recipe_post_type()
     'has_archive'        => true,
     'hierarchical'       => false,
     'menu_position'      => 20,
-    'supports'           => array('title', 'editor', 'author', 'thumbnail', 'excerpt'),
+    'supports'           => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'custom-fields'),
     'show_in_rest'        => true,
     'description'        => __('A custom post type for recipes', 'udemy-plus'),
     'taxonomies'         => ['category', 'post_tag']
@@ -61,5 +61,13 @@ function up_recipe_post_type()
     'single'  => true,
     'show_in_rest'  => true,
     'default'  => '#'
+  ]);
+
+  register_post_meta('recipe', 'recipe_rating', [
+    'type' => 'number',
+    'description' => __('The rating for a recipe', 'udemy-plus'),
+    'single' => true,
+    'default' => 0,
+    'show_in_rest' => true
   ]);
 }
