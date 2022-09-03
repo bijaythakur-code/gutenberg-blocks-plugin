9a45249c26eea41071cb23df132386726922de0c
Bijay Thakur <bijaythakur330@gmail.com>
2022-09-03 17:38:55 +0545

    240 - Adding Columns

--- build/blocks/team-members-group/index.asset.php
+++ build/blocks/team-members-group/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b7758dcb092ce6ffd071');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '814ef73639444c58ff54');
