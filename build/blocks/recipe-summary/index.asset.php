d2d09b807c45ea188e00828ba73e2cfbced77d81
Bijay Thakur <bijaythakur330@gmail.com>
2022-09-03 08:07:25 +0545

    223 - Grabbing Post Metadata

--- build/blocks/recipe-summary/index.asset.php
+++ build/blocks/recipe-summary/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ef30286dda94575483d6');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'dfcf15c0f7068b99160b');
