517cb96d2a73205506346c765d96ebb06ce508ac
Bijay Thakur <bijaythakur330@gmail.com>
2022-09-03 17:51:58 +0545

    241 - Adding the MediaPlaceholder Component

--- build/blocks/team-member/index.asset.php
+++ build/blocks/team-member/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e335b38394993722b726');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c05d8809f8541180f3da');
