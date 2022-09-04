ff7def8e6bb1255254bbf114a75be4e6a7a2bbf7
Bijay Thakur <bijaythakur330@gmail.com>
2022-09-04 11:30:29 +0545

    251 - Quickly Refactoring the Team Member Block

--- build/blocks/team-member/index.asset.php
+++ build/blocks/team-member/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '87765787fd147961d2a7');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8f9726feda11e3882329');
