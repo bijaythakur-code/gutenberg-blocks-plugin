b6f1850b97a229f612d0ce265bef71b1fe8ebd01
Bijay Thakur <bijaythakur330@gmail.com>
2022-09-04 13:18:19 +0545

    257 - Preparing the Social Media Links

--- build/blocks/team-member/index.asset.php
+++ build/blocks/team-member/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c717f5e0585daa49f123');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '07df1b18bbcc114baa7b');
