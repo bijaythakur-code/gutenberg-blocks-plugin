46a56c9fd82298cd6e1726a82c2b78056fe79a54
Bijay Thakur <bijaythakur330@gmail.com>
2022-09-04 11:20:36 +0545

    250 - Fixing Blob Memory Leaks

--- build/blocks/team-member/index.asset.php
+++ build/blocks/team-member/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9a19c64a3cf201a37c8f');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '87765787fd147961d2a7');
--- src/blocks/team-member/index.js
+++ src/blocks/team-member/index.js
@@ -7,7 +7,7 @@ import {
 } from '@wordpress/block-editor'
 import { __ } from '@wordpress/i18n'
 import { PanelBody, TextareaControl, Spinner } from '@wordpress/components'
-import { isBlobURL } from '@wordpress/blob'
+import { isBlobURL, revokeBlobURL } from '@wordpress/blob'
 import { useState } from '@wordpress/element'
 import icons from '../../icons.js'
 import './main.css'
@@ -62,6 +62,8 @@ registerBlockType('udemy-plus/team-member', {
                     imgAlt: img.alt,
                     imgURL: newImgURL,
                   })
+
+                  revokeBlobURL(imgPreview)
                 }
 
                 setImgPreview(newImgURL)
