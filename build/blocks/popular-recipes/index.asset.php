ecc28ee4f1a1806d88504d5a78f6dc3d97bdbe67
Bijay Thakur <bijaythakur330@gmail.com>
2022-09-05 12:18:46 +0545

    272 - Rendering Raw HTML

--- build/blocks/popular-recipes/index.asset.php
+++ build/blocks/popular-recipes/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'aefa78feb0ef597c9fb6');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b262c99f89e9167819e0');
