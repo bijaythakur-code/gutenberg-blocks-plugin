b480eeed9a52f1a650af24b0d12575841001096d
Bijay Thakur <bijaythakur330@gmail.com>
2022-09-05 12:49:33 +0545

    273 - The WPQuery Class

--- includes/blocks/popular-recipes.php
+++ includes/blocks/popular-recipes.php
@@ -2,6 +2,31 @@
 
 function up_popular_recipes_cb()
 {
+  $title = esc_html($atts['title']);
+  $cuisineIDs = array_map(function ($term) {
+    return $term['id'];
+  }, $atts['cuisines']);
+
+  $args = [
+    'post_type' => 'recipe',
+    'posts_per_page'  => $atts['count'],
+    'orderby' => 'meta_value_num',
+    'meta_key'  => 'recipe_rating',
+    'order' => 'DESC'
+  ];
+
+  if (!empty($cuisineIDs)) {
+    $args['tax_query'] = [
+      [
+        'taxonomy' => 'cuisine',
+        'field' => 'term_id',
+        'terms' => $cuisineIDs
+      ]
+    ];
+  }
+
+  $query = new WP_Query($args);
+
   ob_start();
 ?>
 
