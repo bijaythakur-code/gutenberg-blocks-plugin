5b3291c0191c7f0a49aa76d496773194a817ee8b
Bijay Thakur <bijaythakur330@gmail.com>
2022-09-05 14:42:56 +0545

    279 - Finishing the Daily Recipe Block

--- includes/blocks/daily-recipe.php
+++ includes/blocks/daily-recipe.php
@@ -1,13 +1,30 @@
 <?php
 
-function up_daily_recipe_cb()
+function up_daily_recipe_cb($atts)
+
 {
+
+  $title = esc_html($atts['title']);
+  $id = get_transient('up_daily_recipe_id');
+
+  if (!$id) {
+    $id = up_generate_daily_recipe();
+  }
+
   ob_start();
 ?>
 
-  <?php
+  <div class="wp-block-udemy-plus-daily-recipe">
+    <h6><?php echo $title; ?></h6>
+    <a href="<?php the_permalink($id); ?>">
+      <img src="<?php echo get_the_post_thumbnail_url($id, 'full'); ?>" alt="">
+      <h3><?php echo get_the_title($id); ?></h3>
+    </a>
+  </div>
 
-  $output = ob_get_clean();
+<?php
+
+  $output = ob_get_contents();
   ob_end_clean();
 
   return $output;
