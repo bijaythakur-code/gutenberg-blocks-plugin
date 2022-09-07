7d952cabef2dd8ee50c6fb93f6f98b08c055ab20
Bijay Thakur <bijaythakur330@gmail.com>
2022-09-07 07:59:06 +0545

    313 - Preparing a New Format Type

--- build/block-editor/index.asset.php
+++ build/block-editor/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'f6c8a1ec7eefb73cd19c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'db1863b346ec128c61a3');
