e3426f913ffbb4ef9a7266fc1177bd767e1e2121
Bijay Thakur <bijaythakur330@gmail.com>
2022-09-07 09:37:46 +0545

    318 - Loading Translations

---
+++ includes/load-translations.php
@@ -0,0 +1,10 @@
+<?php
+
+function up_load_php_translations()
+{
+  load_plugin_textdomain(
+    "udemy-plus",
+    false,
+    "udemy-plus/languages"
+  );
+}
--- index.php
+++ index.php
@@ -53,3 +53,4 @@ add_action('init', 'up_register_assets');
 add_action('admin_init', 'up_settings_api');
 add_action('enqueue_block_editor_assets', 'up_enqueue_block_editor_assets');
 add_action('wp_head', 'up_wp_head');
+add_action('init', 'up_load_php_translations');
