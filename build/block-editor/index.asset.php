b32c9d09b3715c54aab6d3737f7476edfd263aad
Bijay Thakur <bijaythakur330@gmail.com>
2022-09-07 08:34:30 +0545

    315 - Conditionally Rendering Toolbar Buttons

--- build/block-editor/index.asset.php
+++ build/block-editor/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-rich-text'), 'version' => '717329f5b7d4297b6df8');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-rich-text'), 'version' => '41b78de5b23abf2362ba');
